89b465b6ddb40f8a22c8b783c42d63df1bca2876
uwin5949 <uwin.ma@gmail.com>
2018-05-30 22:40:15 +0530

    api change

--- src/AppBundle/Controller/AppAPIController.php
+++ src/AppBundle/Controller/AppAPIController.php
@@ -46,8 +46,8 @@ class AppAPIController extends BaseController
     public function busListAPIAction(Request $request){
         if($request->getMethod() == 'POST'){
             $route_id = $request->get('route_id');
-            $route=$this->getRepository('RoadRoute')->findOneBy(array('published'=>true,'id'=>$route_id));
-                $buses=$this->getRepository('Bus')->findBy(array('route'=>$route,'published'=>true));
+            $route=$this->getRepository('RoadRoute')->findOneBy(array('id'=>$route_id));
+                $buses=$this->getRepository('Bus')->findBy(array('route'=>$route));
                 $stdBuses=array();
                 for($i=0;$i<count($buses);$i++){
                     $stdBus=new \stdClass();
